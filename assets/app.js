b6f9a61de429df0b07bb759f3caa58d4d4e3f2dc
sarvjot <sarvjotsingh422002@gmail.com>
2021-04-20 12:56:20 +0530

    fix horizontal scrollbar for firefox

--- assets/app.js
+++ assets/app.js
@@ -1,76 +1,90 @@
 /* -----------------------------------------Smooth Scroll effect------------------------------------------------ */
-import jump from './node_modules/jump.js/dist/jump.module.js';
+import jump from "./node_modules/jump.js/dist/jump.module.js";
 document.querySelectorAll(".top-btn").forEach((btn) => {
-    btn.addEventListener('click',() => {
-        jump('#navbar',{
-            duration:1000,
-            offset: 1
+    btn.addEventListener("click", () => {
+        jump("#navbar", {
+            duration: 1000,
+            offset: 1,
         });
     });
-})
-document.querySelector("#home-btn").addEventListener('click',() => {
-    jump('.container-home',{
-        duration:1000,
-        offset: 1
-    });
 });
-document.querySelector("#personalize-btn").addEventListener('click',() => {
-    jump('.container-personalize',{
-        duration:1000,
-        offset: 1
-    });
-});
-document.querySelector("#about-btn").addEventListener('click',() => {
-    jump('.container-AboutMe',{
+document.querySelector("#home-btn").addEventListener("click", () => {
+    jump(".container-home", {
         duration: 1000,
-        offset: 1
+        offset: 1,
     });
 });
-document.querySelector("#technologies-btn").addEventListener('click',() => {
-    jump('.container-technologies',{
+document.querySelector("#personalize-btn").addEventListener("click", () => {
+    jump(".container-personalize", {
         duration: 1000,
-        offset: 1
+        offset: 1,
     });
 });
-document.querySelector("#projects-btn").addEventListener('click',() => {
-    jump('.container-projects',{
+document.querySelector("#about-btn").addEventListener("click", () => {
+    jump(".container-AboutMe", {
         duration: 1000,
-        offset: 1
+        offset: 1,
     });
 });
-document.querySelector("#academics-btn").addEventListener('click',() => {
-    jump('.container-academics',{
+document.querySelector("#technologies-btn").addEventListener("click", () => {
+    jump(".container-technologies", {
         duration: 1000,
-        offset: 1
+        offset: 1,
+    });
+});
+document.querySelector("#projects-btn").addEventListener("click", () => {
+    jump(".container-projects", {
+        duration: 1000,
+        offset: 1,
+    });
+});
+document.querySelector("#academics-btn").addEventListener("click", () => {
+    jump(".container-academics", {
+        duration: 1000,
+        offset: 1,
     });
 });
 /* ------------------------------------------------Glider------------------------------------------------- */
 
-window.addEventListener('load', function(){
-    new Glider(document.querySelector('.glider'), {
+window.addEventListener("load", function () {
+    new Glider(document.querySelector(".glider"), {
         slidesToShow: 1,
         slidesToScroll: 1,
         draggable: true,
-        dots: '.dots',
+        dots: ".dots",
         scrollLock: true,
         arrows: {
-          prev: '.glider-prev',
-          next: '.glider-next'
-        }
-    })
-  })
-/* ------------------------------------------------Image-Popup------------------------------------------------- */
-$('.certificate-btn').magnificPopup({
-    type: 'iframe',
-    gallery:{
-        enabled:true
-    }
+            prev: ".glider-prev",
+            next: ".glider-next",
+        },
+    });
 });
-$('.project-img').magnificPopup({
-    type: 'image',
-    gallery:{
-        enabled:true
+
+document.addEventListener("glider-loaded", hideFFScrollBars);
+document.addEventListener("glider-refresh", hideFFScrollBars);
+function hideFFScrollBars(e) {
+    var scrollbarHeight = 17; // Currently 17, may change with updates
+    if (/firefox/i.test(navigator.userAgent)) {
+        // We only need to appy to desktop. Firefox for mobile uses
+        // a different rendering engine (WebKit)
+        if (window.innerWidth > 575) {
+            e.target.parentNode.style.height =
+                e.target.offsetHeight - scrollbarHeight + "px";
+        }
     }
+}
+/* ------------------------------------------------Image-Popup------------------------------------------------- */
+$(".certificate-btn").magnificPopup({
+    type: "iframe",
+    gallery: {
+        enabled: true,
+    },
+});
+$(".project-img").magnificPopup({
+    type: "image",
+    gallery: {
+        enabled: true,
+    },
 });
 
 // media query event handler
@@ -79,103 +93,105 @@ if (matchMedia) {
     mq.addListener(WidthChange);
     WidthChange(mq);
 }
-    
+
 // media query change
 function WidthChange(mq) {
     if (mq.matches) {
-    // window width is less than 500px
-        $('.certificate-btn').magnificPopup({
-            type: 'image',
-            gallery:{
-                enabled:true
-            }
-        });        
+        // window width is less than 500px
+        $(".certificate-btn").magnificPopup({
+            type: "image",
+            gallery: {
+                enabled: true,
+            },
+        });
     } else {
-    // window width is more than 500px
-        $('.certificate-btn').magnificPopup({
-            type: 'iframe',
-            gallery:{
-                enabled:true
-            }
+        // window width is more than 500px
+        $(".certificate-btn").magnificPopup({
+            type: "iframe",
+            gallery: {
+                enabled: true,
+            },
         });
     }
 }
 /* --------------------------------------Javascript theme Switcher ----------------------------------------------*/
 
-var body = document.querySelector('body');
+var body = document.querySelector("body");
 
 // Applying the cached theme on reload
 
 let color = localStorage.getItem("color");
 let font = localStorage.getItem("font");
 
-if(color){
+if (color) {
     body.classList = [color];
 }
-if(font){
+if (font) {
     body.id = font;
 }
 updateStyle();
 
 // Color Switcher
-document.querySelector("#card-1").addEventListener('click',() => {
-    var body = document.querySelector('body');
-    if(body.classList.contains("aqua") === false){
+document.querySelector("#card-1").addEventListener("click", () => {
+    var body = document.querySelector("body");
+    if (body.classList.contains("aqua") === false) {
         body.classList = ["aqua"];
-        localStorage.setItem('color',"aqua");
+        localStorage.setItem("color", "aqua");
         updateStyle();
     }
-})
-document.querySelector("#card-2").addEventListener('click',() => {
-    var body = document.querySelector('body');
-    if(body.classList.contains("furious") === false){
+});
+document.querySelector("#card-2").addEventListener("click", () => {
+    var body = document.querySelector("body");
+    if (body.classList.contains("furious") === false) {
         body.classList = ["furious"];
-        localStorage.setItem('color',"furious");
+        localStorage.setItem("color", "furious");
         updateStyle();
     }
-})
-document.querySelector("#card-3").addEventListener('click',() => {
-    var body = document.querySelector('body');
-    if(body.classList.contains("peaceful") === false){
+});
+document.querySelector("#card-3").addEventListener("click", () => {
+    var body = document.querySelector("body");
+    if (body.classList.contains("peaceful") === false) {
         body.classList = ["peaceful"];
-        localStorage.setItem('color',"peaceful");
+        localStorage.setItem("color", "peaceful");
         updateStyle();
     }
-})
-document.querySelector("#card-4").addEventListener('click',() => {
-    var body = document.querySelector('body');
-    if(body.classList.contains("lovely") === false){
+});
+document.querySelector("#card-4").addEventListener("click", () => {
+    var body = document.querySelector("body");
+    if (body.classList.contains("lovely") === false) {
         body.classList = ["lovely"];
-        localStorage.setItem('color',"lovely");
+        localStorage.setItem("color", "lovely");
         updateStyle();
     }
-})
+});
 // Font-Style Switcher
-document.querySelector("#handwriting-card-1").addEventListener('click',() => {
-    var body = document.querySelector('body');
-    if(body.id !== "sketchy"){
+document.querySelector("#handwriting-card-1").addEventListener("click", () => {
+    var body = document.querySelector("body");
+    if (body.id !== "sketchy") {
         body.id = "sketchy";
-        localStorage.setItem('font',"sketchy");
+        localStorage.setItem("font", "sketchy");
         updateStyle();
     }
-})
-document.querySelector("#handwriting-card-2").addEventListener('click',() => {
-    var body = document.querySelector('body');
-    if(body.id !== "classic"){
+});
+document.querySelector("#handwriting-card-2").addEventListener("click", () => {
+    var body = document.querySelector("body");
+    if (body.id !== "classic") {
         body.id = "classic";
-        localStorage.setItem('font',"classic");
+        localStorage.setItem("font", "classic");
         updateStyle();
     }
-})
-document.querySelector("#handwriting-card-3").addEventListener('click',() => {
-    var body = document.querySelector('body');
-    if(body.id !== "trendy"){
+});
+document.querySelector("#handwriting-card-3").addEventListener("click", () => {
+    var body = document.querySelector("body");
+    if (body.id !== "trendy") {
         body.id = "trendy";
-        localStorage.setItem('font',"trendy");
+        localStorage.setItem("font", "trendy");
         updateStyle();
     }
-})
+});
 /*------------------------------------------- Current Style Dashboard -------------------------------------------*/
-function updateStyle(){
-    document.querySelector("#current-style").textContent = `${body.classList[0]} - ${body.id}`;
+function updateStyle() {
+    document.querySelector(
+        "#current-style"
+    ).textContent = `${body.classList[0]} - ${body.id}`;
 }
